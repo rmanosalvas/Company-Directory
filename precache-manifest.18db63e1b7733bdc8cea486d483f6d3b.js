08e16f49e9f85185fccf0637f1bdc14c339906fd
Ricardo <manosalvas2013@gmail.com>
2020-10-13 11:56:00 -0400

    Updates

---
+++ precache-manifest.18db63e1b7733bdc8cea486d483f6d3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2659a88f588443720199909de117057",
+    "url": "/Company-Directory/index.html"
+  },
+  {
+    "revision": "59115943126eb3762ab1",
+    "url": "/Company-Directory/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "6bb731a7dfd757f981f6",
+    "url": "/Company-Directory/static/css/main.ced6bf89.chunk.css"
+  },
+  {
+    "revision": "59115943126eb3762ab1",
+    "url": "/Company-Directory/static/js/2.b0119d46.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Company-Directory/static/js/2.b0119d46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bb731a7dfd757f981f6",
+    "url": "/Company-Directory/static/js/main.917104bd.chunk.js"
+  },
+  {
+    "revision": "762cf5d3f96fd5f81e36",
+    "url": "/Company-Directory/static/js/runtime-main.e6c126fa.js"
+  }
+]);
